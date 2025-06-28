7de2e99f168cde3e628d85c60568af3a7f784645
SIVANESHAKUMAR K S <ks.sivaneshakumar@gmail.com>
2025-06-28 22:46:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -62,4 +62,7 @@ def download_log():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(debug=False, host='0.0.0.0', port=port)
+
